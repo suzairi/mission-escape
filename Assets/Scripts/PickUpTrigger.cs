9e43b472f71a5f006ef38eed5fca74a2842bab3a
suzairi <selma.uzairi@htl-salzburg.ac.at>
2019-03-18 10:56:37 +0100

    first commit

---
+++ Assets/Scripts/PickUpTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpTrigger : MonoBehaviour {
+
+	  public float speed;
+
+    private Rigidbody2D rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+	Debug.Log ("Collider"+other.name+" "+gameObject.name);
+        if (other.gameObject.CompareTag ("Diamond"))
+        {
+            other.gameObject.SetActive (false);
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class PlayerController : MonoBehaviour
+ {
+     public float speed = 5f;
+ 
+     private Rigidbody2D rb2d;
+ 
+     void Start()
+     {
+         rb2d = GetComponent<Rigidbody2D>();
+     }
+     void Update()
+     {
+         if (Input.GetKey(KeyCode.A))
+         {
+             transform.position += Vector3.left * speed * Time.deltaTime;
+         }
+ 
+         if (Input.GetKey(KeyCode. D))
+         {
+             transform.position += Vector3.right * speed * Time.deltaTime;
+	}
+
+     }
+ }
